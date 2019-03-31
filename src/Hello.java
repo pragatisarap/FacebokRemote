cd7a7500875566ae05006d5e7168789204fb1edb
pragatisarap <pragatisarap1998@gmail.com>
2019-03-30 17:01:01 -0700

    my first commit

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Word");
+		System.out.println("my initial commit")
+		System.out.println("my first commit")
 	}
 
 }
